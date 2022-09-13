9e232ed796561a31554b28f949c48f2984ad591c
YTLogos <1051927088@qq.com>
2022-09-13 18:42:15 +0800

    update citation

--- render_pdf.R
+++ render_pdf.R
@@ -6,5 +6,6 @@ rmarkdown::render("index.rmd",
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "taoyan_cv.pdf")
+                       output = "taoyan_cv.pdf",
+                       browser = "D://software/Google/Chrome/Application/chrome.exe")
 
