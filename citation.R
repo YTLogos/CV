a7fff894fd24c2b356e8726e5189d200f66161eb
YTLogos <tyan@hunau.edu.cn>
2025-11-24 10:59:49 +0800

    update citation

--- citation.R
+++ citation.R
@@ -10,8 +10,6 @@ loadfonts(device = "win")
 
 theme_set(theme_get() + theme(text = element_text(family = 'Times New Roman')))
 
-#Sys.setenv(http_proxy="http://127.0.0.1:43723")
-
 
 id <- '8p3wQNwAAAAJ'
 
@@ -25,13 +23,11 @@ cites <- tryCatch(get_citation_history(id), error = function(e) return(NULL))
 
 if (is.null(cites)) {
     cites <- tinyscholar::tinyscholar(id)$citation
-
     profile = jsonlite::fromJSON("profile.json")
+    profile$date <- Sys.Date()
     if (profile$total_cites < cites[1,2])
         profile$total_cites <- cites[1,2]
     cat(toJSON(profile), file ="profile.json")
-
-
     cites <- cites[-1, ] # remove 'total' row
     names(cites) <- c("year", "cites")
     cites$year <- as.numeric(cites$year)
@@ -40,7 +36,6 @@ if (is.null(cites)) {
 if (!is.null(cites)) {
     cat(toJSON(cites), file = "citation.json")
 }
-
 cites <- fromJSON("citation.json")
 cites <- slice(cites, tail(row_number(), 6))
 cites$year <- factor(cites$year)
@@ -53,16 +48,9 @@ p <- ggplot(cites, aes(cites, year)) +
     scale_x_continuous(position="top") +
     theme_minimal(base_size=10) + xlab(NULL) + ylab(NULL) +
     theme(panel.grid.major.y = element_blank(), 
-          panel.grid.minor = element_blank(),
-          panel.grid.major.x = element_line(linetype="dashed"),
-          plot.caption=element_text(colour='grey30', size=7, vjust = 1)) +
+        panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_line(linetype="dashed"),
+        plot.caption=element_text(colour='grey30', size=7, vjust = 1)) +
     theme_transparent() 
 
 ggsave(p, file = "citation.png", width=2, height=2, bg = "transparent")
-
-
-## library(magick)
-## p <- image_read("citation.png")
-## p <- image_transparent(p, "white")
-## image_write(p, path="citation.png")
-
