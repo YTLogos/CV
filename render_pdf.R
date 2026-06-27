fb98706f171479948787adfb3465dc0d27ca3718
YTLogos <tyan@hunau.edu.cn>
2026-06-27 16:41:06 +0800

    update citation

--- render_pdf.R
+++ render_pdf.R
@@ -1,11 +1,15 @@
 cache_data <- TRUE
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
 rmarkdown::render("index.rmd",
-                  params = list(pdf_mode = TRUE, cache_data = cache_data),
-                  output_file = tmp_html_cv_loc)
+  params = list(pdf_mode = TRUE, cache_data = cache_data),
+  output_file = tmp_html_cv_loc
+)
 
 # Convert to PDF using Pagedown
-pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "taoyan_cv.pdf",
-                       browser = "C:/Program Files/Google/Chrome/Application/chrome.exe")
 
+webshot2::webshot(
+  url = tmp_html_cv_loc,
+  file = "taoyan_cv.pdf",
+  vwidth = 1200,
+  delay = 2
+)
